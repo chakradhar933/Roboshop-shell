d46a22f576b9ab07bd037095a5f90fca38e48c97
ram <chakradhar.thatikonda933@gmail.com>
2023-03-07 17:28:07 +0530

    print colour

--- Frontend.sh
+++ Frontend.sh
@@ -1,5 +1,8 @@
+echo -e "\e[35minstalling nginx\e[0m"
 yum install nginx -y
+echo -e "\e[35mremove old content\e[0m"
 rm -rf /usr/share/nginx/html/*
+echo -e "\e[31mdownload the content\e[0m"
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip
