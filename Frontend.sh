92f4bb9d6f2ed0df04f871269ca60900715a4dd3
ram <chakradhar.thatikonda933@gmail.com>
2023-03-14 17:31:23 +0530

    print colour

--- Frontend.sh
+++ Frontend.sh
@@ -7,7 +7,7 @@ rm -rf /usr/share/nginx/html/*
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip
-cp ${code}/configs/nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf
+cp ${pwd}/configs/nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf
 systemctl enable nginx
 systemctl restart nginx
 
