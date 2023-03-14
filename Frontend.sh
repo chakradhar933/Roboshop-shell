d90f36873f197687cc662e71c26b67a07b6c3d9f
ram <chakradhar.thatikonda933@gmail.com>
2023-03-14 17:56:11 +0530

    redirectores

--- Frontend.sh
+++ Frontend.sh
@@ -1,14 +1,16 @@
 code_dir=$(pwd)
+log_file=/tmp/roboshop.log
+rm -f ${log_file}
 echo -e "\e[31minstalling nginx\e[0m"
-yum install nginx -y &>/tmp/out
+yum install nginx -y &>/tmp/out &>>{log_file}
 #echo -e "\e[35mremove old content\e[0m"
-rm -rf /usr/share/nginx/html/*
+rm -rf /usr/share/nginx/html/* &>>{log_file}
 #echo -e "\e[31mdownload the content\e[0m"
-curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>>{log_file}
 cd /usr/share/nginx/html
-unzip /tmp/frontend.zip
-cp ${code_dir}/configs/nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf
-systemctl enable nginx
-systemctl restart nginx
+unzip /tmp/frontend.zip &>>{log_file}
+cp ${code_dir}/configs/nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf & >>{log_file}
+systemctl enable nginx &>>{log_file}
+systemctl restart nginx &>>{log_file}
 
 #
